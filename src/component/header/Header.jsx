b4a176cac4930d960bd35b0e39ad57c23ecf8a36
Alexandre <alexmiquel888@gmail.com>
2025-01-21 11:59:14 +0100

    Modif des li

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -8,12 +8,12 @@ const Header = () => {
           <h1 className="navbar__title">Portfolio</h1>
           <ul className="navbar__menu">
             <li className="navbar__menu-item">Présentation</li>
-            <li className="navbar__menu-item">Mes Projets</li>
-            <li className="navbar__menu-item">Mes Compétences</li>
-            <li className="navbar__menu-item">Mon GitHub</li>
-            <li className="navbar__menu-item">Mes formations</li>
-            <li className="navbar__menu-item">Mes Réseaux Sociaux</li>
-            <li className="navbar__menu-item">Me Contacter</li>
+            <li className="navbar__menu-item">Projets</li>
+            <li className="navbar__menu-item">Compétences</li>
+            <li className="navbar__menu-item">GitHub</li>
+            <li className="navbar__menu-item">formations</li>
+            <li className="navbar__menu-item">Réseaux Sociaux</li>
+            <li className="navbar__menu-item">Contact</li>
           </ul>
         </div>
       </nav>
