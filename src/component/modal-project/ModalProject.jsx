b29885ec8ff7a5c584e980bec8193c3603804308
Alexandre <alexmiquel888@gmail.com>
2025-01-22 21:28:42 +0100

    Update ModalProject.jsx

--- src/component/modal-project/ModalProject.jsx
+++ src/component/modal-project/ModalProject.jsx
@@ -3,7 +3,13 @@ import { PropTypes } from 'prop-types';
 const ModalProject = ({ url }) => {
   return (
     <div className="wrapper">
-      <iframe className="wrapper__modal" src={url} allowFullScreen></iframe>
+      <iframe
+        className="wrapper__modal"
+        src={url}
+        width={1200}
+        height={800}
+        allowFullScreen
+      ></iframe>
     </div>
   );
 };
