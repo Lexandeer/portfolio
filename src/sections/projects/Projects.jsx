bf369f28c462c24b473f8fdb56b6b9ba4b5e3105
Alexandre <alexmiquel888@gmail.com>
2025-01-23 21:20:51 +0100

    Update Projects.jsx

--- src/sections/projects/Projects.jsx
+++ src/sections/projects/Projects.jsx
@@ -1,44 +1,17 @@
 import './Projects.scss';
-import { useState } from 'react';
+
 import ModalProject from '../../component/modal-project/ModalProject';
 import DoubleTitle from '../../component/DoubleTitle/DoubleTitle';
 
 const Projects = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const toggleModal = () => {
-    setIsModalOpen(!isModalOpen);
-  };
-
   return (
     <section className="projects-wrapper">
       <DoubleTitle
         primaryTitle={'Projets'}
         secondaryTitle={'Découvrez mes créations'}
       />
-      <div>
-        <button
-          type="button"
-          onDoubleClick={toggleModal}
-          className="open-button"
-        >
-          View Project
-        </button>
-        {isModalOpen && (
-          <div className="overlay">
-            <div className="overlay__modal">
-              <button
-                type="button"
-                onClick={toggleModal}
-                className="overlay__modal--close"
-              >
-                close
-              </button>
-              <ModalProject url="https://lexandeer.github.io/Site_Ohmyfood/" />
-            </div>
-          </div>
-        )}
-      </div>
+      <ModalProject url="https://lexandeer.github.io/Site_Ohmyfood/" />;{' '}
+      {/* 3x ModalProject pour les 3 articles */}
     </section>
   );
 };
