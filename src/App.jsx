ce64b12cfa579ec24f27af10e9855c150b33a2d7
Alexandre <alexmiquel888@gmail.com>
2025-01-29 12:17:10 +0100

    Ajout des composant GithubStat et Formation à l'App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from './component/Navbar/Navbar';
 import Presentation from './sections/presentation/Presentation';
 import Projects from './sections/projects/Projects';
 import SkillSet from './sections/skill-set/SkillSet';
+import GithubStats from './sections/githubStats/GithubStats';
+import Formation from './sections/formation/Formation';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Presentation />
       <Projects />
       <SkillSet />
+      <GithubStats />
+      <Formation />
     </>
   );
 }
