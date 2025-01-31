d3e4c19e2c7b7a5b2dd4fd746f02d88c8d720bc5
Alexandre <alexmiquel888@gmail.com>
2025-01-31 12:45:07 +0100

    Update Navbar.jsx

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -2,32 +2,31 @@ import './Navbar.scss';
 
 const Header = () => {
   return (
-    <header>
-      <nav className="navbar">
-        <div className="navbar__container">
-          <h1 className="navbar__title">Portfolio</h1>
-          <ul className="navbar__menu">
-            <li className="navbar__menu-item">
-              <a href="#presentation">Présentation</a>
-            </li>
-            <li className="navbar__menu-item">
-              <a href="#projects">Projets</a>
-            </li>
-            <li className="navbar__menu-item">
-              <a href="#skillset">Compétences</a>
-            </li>
-            <li className="navbar__menu-item">
-              <a href="#github">GitHub</a>
-            </li>
-            <li className="navbar__menu-item">
-              <a href="#formation">formations</a>
-            </li>
-            <li className="navbar__menu-item">Réseaux Sociaux</li>
-            <li className="navbar__menu-item">Contact</li>
-          </ul>
-        </div>
-      </nav>
-    </header>
+    <nav className="navbar">
+      <div className="navbar__container">
+        <h1 className="navbar__title">Portfolio</h1>
+        <ul className="navbar__menu">
+          <li className="navbar__menu-item">
+            <a href="#presentation">Présentation</a>
+          </li>
+          <li className="navbar__menu-item">
+            <a href="#projects">Projets</a>
+          </li>
+          <li className="navbar__menu-item">
+            <a href="#skillset">Compétences</a>
+          </li>
+          <li className="navbar__menu-item">
+            <a href="#github">GitHub</a>
+          </li>
+          <li className="navbar__menu-item">
+            <a href="#formation">formations</a>
+          </li>
+          <li className="navbar__menu-item">
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
