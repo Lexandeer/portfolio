61f8baef3aa022badd74e71f5abdc10ff50a568c
Alexandre <alexmiquel888@gmail.com>
2025-01-31 12:45:13 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import './App.css';
-import Navbar from './component/Navbar/Navbar';
 import Presentation from './sections/presentation/Presentation';
 import Projects from './sections/projects/Projects';
 import SkillSet from './sections/skill-set/SkillSet';
 import GithubStats from './sections/githubStats/GithubStats';
 import Formation from './sections/formation/Formation';
+import Header from './sections/header/Header';
+import Footer from './sections/footer/Footer';
+import Contact from './sections/contact/Contact';
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Header />
       <Presentation />
       <Projects />
       <SkillSet />
       <GithubStats />
       <Formation />
+      <Contact />
+      <Footer />
     </>
   );
 }
