f1263ab20c996b0e50721710847041f782714527
Alexandre <alexmiquel888@gmail.com>
2025-01-31 12:44:50 +0100

    Create Header.jsx

---
+++ src/sections/header/Header.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../../component/Navbar/Navbar';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
