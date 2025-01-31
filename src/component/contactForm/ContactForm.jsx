84202f93a3a533d302605967f9536ca4d4c24dd1
Alexandre <alexmiquel888@gmail.com>
2025-01-31 12:45:11 +0100

    Create ContactForm.jsx

---
+++ src/component/contactForm/ContactForm.jsx
@@ -0,0 +1,11 @@
+import './ContactForm.scss';
+
+const ContactForm = () => {
+  return (
+    <div className="contact-form">
+      <form className="contact-form__formulaire"></form>
+    </div>
+  );
+};
+
+export default ContactForm;
