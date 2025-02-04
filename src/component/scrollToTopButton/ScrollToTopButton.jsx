5662f832be041812975426d4d13150e976beabfa
Alexandre <alexmiquel888@gmail.com>
2025-02-04 13:20:08 +0100

    Création d'un composant ScrollToTop
    Permet de revenir en haut de la page

---
+++ src/component/scrollToTopButton/ScrollToTopButton.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+import './ScrollToTopButton.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+
+const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 1000) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <button
+      className={`scroll-to-top ${isVisible ? 'visible' : ''}`}
+      onClick={scrollToTop}
+    >
+      <FontAwesomeIcon icon={faArrowUp} />
+    </button>
+  );
+};
+
+export default ScrollToTopButton;
