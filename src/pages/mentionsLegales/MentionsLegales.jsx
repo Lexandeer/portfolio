d49ae070095aca3a90b4e31c104f3b3a9e8eb23c
Alexandre <alexmiquel888@gmail.com>
2025-02-05 10:47:26 +0100

    Create MentionsLegales.jsx

---
+++ src/pages/mentionsLegales/MentionsLegales.jsx
@@ -0,0 +1,15 @@
+import './MentionsLegales.scss';
+
+const MentionsLegales = () => {
+  return (
+    <div className="legal-container">
+      <h1>Mentions Légales</h1>
+      <p>Ce site est édité par [Ton Nom ou Nom de l'Entreprise].</p>
+      <p>Adresse : [Ton Adresse]</p>
+      <p>Hébergeur : [Nom de l'hébergeur, Adresse]</p>
+      <p>Contact : [Ton Email ou Formulaire de contact]</p>
+    </div>
+  );
+};
+
+export default MentionsLegales;
