2a4da43074be445209f05567fa2a4bee4bf3f16e
Alexandre <alexmiquel888@gmail.com>
2025-02-05 10:46:24 +0100

    Ajout d'une logique de resize
    Pour changer le lien de la vidéo en fonction de la taille de l'appareil utilisé

--- src/sections/projects/Projects.jsx
+++ src/sections/projects/Projects.jsx
@@ -1,9 +1,20 @@
 import './Projects.scss';
-
+import { useEffect, useState } from 'react';
 import ModalProject from '../../component/modal-project/ModalProject';
 import DoubleTitle from '../../component/DoubleTitle/DoubleTitle';
 
 const Projects = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 530); //State qui change le lien de la vidéo en fonction de la taille d'écran utilisé
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 530);
+    };
+
+    window.addEventListener('resize', handleResize); // On ajoute un écouteur d'évenement sur le changement de taille de l'écran
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
   return (
     <section className="wrapper" id="projects">
       <DoubleTitle
@@ -22,7 +33,11 @@ const Projects = () => {
         />
         <ModalProject
           src="Pictures\ArgentBank-Picture.png"
-          vSrc="Videos/ArgentBank-video.mp4"
+          vSrc={
+            isMobile
+              ? 'Videos/Argent-bank-mobile-video.mp4'
+              : 'Videos/ArgentBank-video.mp4'
+          }
           alt="Preview du site ArgentBank"
           title="ArgentBank"
           description="Un tableau de bord permettant aux utilisateurs d'une nouvelle banque en ligne de s'authentifier, gérer leur profil"
@@ -31,7 +46,9 @@ const Projects = () => {
         />
         <ModalProject
           src="Pictures\Kasa-Picture.png"
-          vSrc="Videos/Kasa-video.mp4"
+          vSrc={
+            isMobile ? 'Videos/Kasa-mobile-video.mp4' : 'Videos/Kasa-video.mp4'
+          }
           alt="Preview du site Kasa"
           title="Kasa"
           description="Un site responsive permettant aux utilisateurs de réserver des appartements entre particuliers en ligne, avec une interface moderne et une gestion des annonces simplifiée."
