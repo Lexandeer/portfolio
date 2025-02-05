0ef191b0af5cd790dd868ec616265f43feb4c0e6
Alexandre <alexmiquel888@gmail.com>
2025-02-05 10:47:23 +0100

    Create PolitiqueConfidentialite.jsx

---
+++ src/pages/politiqueConfidentialite/PolitiqueConfidentialite.jsx
@@ -0,0 +1,23 @@
+import './PolitiqueConfidentialite.scss';
+
+const PolitiqueConfidentialite = () => {
+  return (
+    <div className="legal-container">
+      <h1>Politique de Confidentialité</h1>
+      <p>
+        Nous collectons certaines informations personnelles dans le cadre de
+        notre formulaire de contact.
+      </p>
+      <p>
+        Les données collectées ne seront jamais partagées avec des tiers sans
+        votre consentement.
+      </p>
+      <p>
+        Vous avez le droit d'accès, de rectification et de suppression de vos
+        données en nous contactant.
+      </p>
+    </div>
+  );
+};
+
+export default PolitiqueConfidentialite;
