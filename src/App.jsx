2747da348eab9524d762d19ac4adb56da54df4af
Alexandre <alexmiquel888@gmail.com>
2025-02-05 21:36:46 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <Routes>
         {/* Page principale */}
         <Route
-          path="/"
+          path="/portfolio"
           element={
             <main>
               <Presentation />
